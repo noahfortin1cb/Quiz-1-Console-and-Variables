f280d6cb58f39bf3a2e30b26f114153b16d31243
noahfortin1cb <noah.fortin@stu.ocsb.ca>
2024-03-21 13:31:28 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -21,31 +21,50 @@ else {
 //Program 2: Grade Calculator
 
 let grade = prompt("What was your percentage in the course? ");
-
+//Creates a variable to store the grade
 if (grade >= 90) {
     console.log("You got an A!")
 }
-
+//Checks if the grade is over 90 for an A
 else if (grade >= 80) {
     console.log("You got a B.")
 }
-
+//Checks if the grade is over 80 for a B
 else if (grade >= 70) {
     console.log("You got a C...")
 }
-
+//Checks if the grade is over 70 for a C
 else if (grade >= 60) {
     console.log("You got a D?")
 }
-
+//Checks if the grade is over 60 for a D
 else if (grade < 60) {
     console.log("You got a F. You will have to retake the course next semester.")
 }
-
+//Checks if the grade is below 60 for an F
 //Program 3: Leap Year Checker
 
 year = prompt("Enter a year, I'll check if it's a leap one. ")
 leap = year % 4
-if ("")
+if (leap = 1) {
+    console.log(year,"is a leap year!")
+}
+else {
+    console.log(year, "is not a leap year.")
+}
 
 //Program 4: Largest Number Finder
+
+num1 = prompt("Input a big number: ")
+num2 = prompt("Input a small number: ")
+ans = num1 - num2
+
+if (ans > 0){
+    console.log("Good job at following instructions, there is a "+ans,"difference between your numbers.")
+}
+else if (ans = 0){
+    console.log("Hey! You just entered the same number twice!")
+}
+else{
+    console.log("These instructions weren't that hard to follow... do better next time.")
+}
